4ef83b59def8db8f55d884b79d64f8cd0972b1b2
receiptdev <receipt.defy@me.com>
2018-07-05 11:59:30 +0900

    Like, Unlike Photos

--- frontend/src/redux/modules/photos.js
+++ frontend/src/redux/modules/photos.js
@@ -3,12 +3,28 @@ import { actionCreators as userActions } from "./user";
 
 // action
 const SET_FEED = "SET_FEED";
+const LIKE_PHOTO = "LIKE_PHOTO";
+const UNLIKE_PHOTO = "UNLIKE_PHOTO";
 
 // action creators
 function setFeed(feed) {
     return {
         type: SET_FEED,
         feed
+    };
+}
+
+function doLikePhoto(photoId) {
+    return {
+        type: LIKE_PHOTO,
+        photoId
+    };
+}
+
+function doUnLikePhoto(photoId) {
+    return {
+        type: UNLIKE_PHOTO,
+        photoId
     };
 }
 
@@ -33,6 +49,48 @@ function getFeed() {
     };
 }
 
+function likePhoto(photoId) {
+    return (dispatch, getState) => {
+        dispatch(doLikePhoto(photoId));
+        const {
+            user: { token }
+        } = getState();
+        fetch(`/images/${photoId}/likes/`, {
+            method: "POST",
+            headers: {
+                Authorization: `JWT ${token}`
+            }
+        }).then(response => {
+            if (response.status === 401) {
+                dispatch(userActions.logout());
+            } else if (!response.ok) {
+                dispatch(doUnLikePhoto(photoId));
+            }
+        });
+    };
+}
+
+function unlikePhoto(photoId) {
+    return (dispatch, getState) => {
+        dispatch(doUnLikePhoto(photoId));
+        const {
+            user: { token }
+        } = getState();
+        fetch(`/images/${photoId}/unlikes/`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `JWT ${token}`
+            }
+        }).then(response => {
+            if (response.status === 401) {
+                dispatch(userActions.logout());
+            } else if (!response.ok) {
+                dispatch(doLikePhoto(photoId));
+            }
+        });
+    };
+}
+
 // initial state
 const initialState = {};
 
@@ -41,6 +99,10 @@ function reducer(state = initialState, action) {
     switch (action.type) {
         case SET_FEED:
             return applySetFeed(state, action);
+        case LIKE_PHOTO:
+            return applyLikePhoto(state, action);
+        case UNLIKE_PHOTO:
+            return applyUnLikePhoto(state, action);
         default:
             return state;
     }
@@ -55,9 +117,47 @@ function applySetFeed(state, action) {
     };
 }
 
+function applyLikePhoto(state, action) {
+    const { photoId } = action;
+    const { feed } = state;
+    const updatedFeed = feed.map(photo => {
+        if (photo.id === photoId) {
+            return {
+                ...photo,
+                is_liked: true,
+                like_count: photo.like_count + 1
+            };
+        }
+
+        return photo;
+    });
+
+    return { ...state, feed: updatedFeed };
+}
+
+function applyUnLikePhoto(state, action) {
+    const { photoId } = action;
+    const { feed } = state;
+    const updatedFeed = feed.map(photo => {
+        if (photo.id === photoId) {
+            return {
+                ...photo,
+                is_liked: false,
+                like_count: photo.like_count - 1
+            };
+        }
+
+        return photo;
+    });
+
+    return { ...state, feed: updatedFeed };
+}
+
 // exports
 const actionCreators = {
-    getFeed
+    getFeed,
+    likePhoto,
+    unlikePhoto
 };
 
 export { actionCreators };
