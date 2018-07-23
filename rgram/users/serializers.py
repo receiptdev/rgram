4c6c565cb06757a3ab719fd588a27cdd3d9ca7b3
receiptdev <receipt.defy@me.com>
2018-07-23 15:31:59 +0900

    Serializer changes

--- rgram/users/serializers.py
+++ rgram/users/serializers.py
@@ -38,6 +38,11 @@ class ListUserSerializer(serializers.ModelSerializer):
             'profile_image',
             'username',
             'name',
+            'bio',
+            'website',
+            'post_count',
+            'followers_count',
+            'following_count',
             'following'
         )
 
