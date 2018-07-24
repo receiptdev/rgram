46c9d02759dfbf216a81a7cbf9674ca0171059bc
receiptdev <receipt.defy@me.com>
2018-07-24 11:29:44 +0900

    Serializer changes

--- rgram/notifications/views.py
+++ rgram/notifications/views.py
@@ -8,7 +8,7 @@ class Notifications(APIView):
     def get(self, request, format=None):
         user = request.user
         notifications = models.Notification.objects.filter(to=user)
-        serializer = serializers.NotificationSerializer(notifications, many=True)
+        serializer = serializers.NotificationSerializer(notifications, many=True, context={'request': request})
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
