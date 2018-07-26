ee2e6a68f23c0247b98fa14de26d91fb0851f849
receiptdev <receipt.defy@me.com>
2018-07-26 20:03:10 +0900

    Change settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,13 +6,8 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-#ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['*'])
-ALLOWED_HOSTS = [
-    "localhost",
-    "0.0.0.0",
-    "127.0.0.1",
-    "*"
-]
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=[
+                         'localhost', 'rgram.ap-northeast-2.elasticbeanstalk.com', 'ad.sunnyworks.co.kr'])
 
 
 # DATABASES
