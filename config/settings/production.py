3b570eaff7fcee99112cb6894a45ba1d796e165b
receiptdev <receipt.defy@me.com>
2018-07-26 22:18:30 +0900

    change settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,7 @@ from .base import env
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 # ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['rgram.ap-northeast-2.elasticbeanstalk.com', 'ad.sunnyworks.co.kr'])
-ALLOWED_HOSTS = ['*"
+ALLOWED_HOSTS = ['*']
 
 
 # DATABASES
