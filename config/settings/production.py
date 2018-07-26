33b22cc04d08d322786946c6a7e13b058bd6bc48
receiptdev <receipt.defy@me.com>
2018-07-26 18:22:01 +0900

    Change settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -27,10 +27,14 @@ DATABASES = {
 
 # CACHES
 # ------------------------------------------------------------------------------
+REDIS_LOCATION = '{0}/{1}'.format(env('REDIS_URL',
+                                      default='redis://127.0.0.1:6379'), 0)
+
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': env('REDIS_URL'),
+        'LOCATION': REDIS_LOCATION,
+        # 'LOCATION': env('REDIS_URL'),
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
             # Mimicing memcache behavior.
