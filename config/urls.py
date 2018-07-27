1ae76e33d22ed817b7f7cccf1ec774f3ccabfc36
receiptdev <receipt.defy@me.com>
2018-07-27 10:22:42 +0900

    urls.py pattern change

--- config/urls.py
+++ config/urls.py
@@ -30,7 +30,8 @@ urlpatterns = [
     ),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
-    re_path(r'^.*/$', views.ReactAppView.as_view())
+    #re_path(r'^.*/$', views.ReactAppView.as_view())
+    url(r'^', views.ReactAppView.as_view()),
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
